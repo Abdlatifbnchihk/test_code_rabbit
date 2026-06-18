461458e19c68ccd194d079a5e8eebea021bf511b
abdellatifbencheikh43-cell <EMAIL_OF_abdellatifbencheikh43-cell>
2026-06-18 10:55:39 +0200

    first test

---
+++ Controllers/testController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function test(Request $request)
+    {
+        if ($user = null) {
+            return false;
+        }
+    }
+}
---
+++ Views/Layout.php
@@ -0,0 +1,20 @@
+<?php
+
+// $host = 'localhost';
+// $databaseName = 
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>
+    <button type="button" class="btn btn-primary">Primary</button>
+</body>
+</html>
